28657e6f5ffa09bc612f828721b062d788b9c9de
numberwang55 <cban0901@hotmail.com>
2024-04-23 09:19:09 +0100

    Feature: Added basic rendering for CarForm, CarList, CarSearch, and CarValue components.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
+
 export default function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <CarForm/>
+      <CarList/>
+      <CarSearch/>
+      <CarValue/>
+    </div>
+  )
 }
