3a16945137c5e1cb8f0fb90f4cffc3e9d80b954e
numberwang55 <cban0901@hotmail.com>
2024-04-23 10:38:39 +0100

    Feature: added redux slices

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+  name: "cars",
+  initialState: {
+    searchTerm: "",
+    cars: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action) {
+      state.cars.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(),
+      });
+    },
+    removeCar(state, action) {
+      // Assumption:
+      // action.payload === id of car to remove
+      const updated = state.cars.filter((car) => {
+        return car.id !== action.payload;
+      });
+      state.cars = updated;
+    },
+  },
+});
+
+export const carsReducer = carsSlice.reducer;
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
